607e8ee3818cf1bede60fed77e7b771a5b317f83
AndriyGoncharuk <160170957+AndriyGoncharuk@users.noreply.github.com>
2024-10-26 22:14:52 +0300

    hw

---
+++ src/db/models/Contact.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      enum: ["work", "personal", "home"],
+      default: "personal",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ContactCollection = model("contact", contactSchema);
